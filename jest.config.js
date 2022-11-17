7896f58b9b672d6385cfe1f9605d13ff74b30ddf
McKayla Washburn <mckayla@hey.com>
2022-11-16 19:26:31 -0700

    tweak config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ export default {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	testPathIgnorePatterns: ["<rootDir>/build/"],
+	collectCoverage: true,
+	coverageReporters: ["text", "cobertura"],
 };
