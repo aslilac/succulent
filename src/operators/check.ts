a51844294816ba1d93d48730a4dd9c8d37ce17d7
McKayla Washburn <mckayla@hey.com>
2024-06-01 11:35:05 -0600

    `CheckError`

--- src/operators/check.ts
+++ src/operators/check.ts
@@ -1,13 +1,19 @@
 import { Schema, SchemaBase } from "../schema.js";
 
 /**
+ * When catching errors, you can use something like `error instanceof CheckError` to determine if
+ * the error is from a failed call to `check`.
+ */
+export class CheckError extends TypeError {}
+
+/**
  * @param x The value to check
  * @param schema The schema to check against
  * @throws {TypeError} when the provided value does not match the schema
  */
 export function check<T>(x: unknown, schema: SchemaBase<T>): asserts x is T {
 	if (!Schema.check(schema, x)) {
-		throw new Error("check returned false instead of throwing, which is bad");
+		throw new CheckError("check returned false instead of throwing, which is bad");
 	}
 }
 
